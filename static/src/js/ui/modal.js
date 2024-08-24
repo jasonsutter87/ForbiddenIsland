3210411efa9e60914d416f647cb25425bb6d5721
jasonsutter87 <jasonsutter87@gmail.com>
2024-08-23 17:46:27 -0700

    player piece moves

--- static/src/js/ui/modal.js
+++ static/src/js/ui/modal.js
@@ -22,10 +22,66 @@ export let StartGameModal = () => {
   });
 };
 
-export let playerMoveOrActionModal = () => {
+export let playerMoveOrActionModal = (toId) => {
   return new Promise((resolve) => {
-    console.log("playerMoveOrActionModal TODO")
+    let fromId = $(`.player-active-${game_details.current_player}`).attr('cardid')
+
+    if(fromId != toId ) {
+      movePlayer(fromId, toId)
+    }
+    resolve();
+  })
+}
+
+
+export let movePlayer = (fromId, toId) => {
+  return new Promise((resolve) => {
+    var $clickedtitle = $(`.player-active-${game_details.current_player}`).children()
+    var $player; 
+
+    $clickedtitle.each((ind, val) => {
+      if($(val).hasClass('player-piece')){
+        $player = $(val);   
+      }
+    });
+
+    var $fromCell = $(`.tile[cardid="${fromId}"]`);
+    var $toCell = $(`.tile[cardid="${toId}"]`);
+
+    if(fromId === toId){
+      resolve();
+    } else {
+
+    // Clone player
+    var $clone = $player.clone().appendTo($('body'));
+
+
+    $clone.css({
+        top: $player.offset().top,
+        left: $player.offset().left,
+        zIndex: 1000
+    });
+
+    // Calculate target position
+    var targetOffset = $toCell.offset();
+
+
+    $($fromCell).removeClass(`player-active-${game_details.current_player}`)
+    $($toCell).addClass(`player-active-${game_details.current_player}`)
+
+
+    // Animate the clone to the new position
+    $clone.animate({
+        top: targetOffset.top,
+        left: targetOffset.left
+    }, 500, function() {
+        // After animation completes, move the real player and remove the clone
+        $clone.remove();
+        $player.appendTo($toCell);
+    });
 
     resolve();
+    }
+
   })
 }
--- static/src/js/ui/ui.js
+++ static/src/js/ui/ui.js
@@ -1,4 +1,5 @@
 //Handles user interface interactions, updating the display, managing the HUD, and responding to player input.
+import { playerMoveOrActionModal } from '/src/js/ui/modal.js'
 
 export function createBoardUI(board) {
   return new Promise(resolve => {
@@ -20,6 +21,7 @@ export function createBoardUI(board) {
         } else if (tileData.flooded) {
             tile.setAttribute('cardId' , tileData.id); 
             tile.classList.add('flooded');
+            tile.onclick = () => playerMoveOrActionModal(tileData.id);
             const img = document.createElement('img');
               img.src = `./assets/images/flooded-tiles/${tileData.slug}.jpeg`; // Adjust the extension if needed
               img.alt = tileData.name; // Optional: add alt text for accessibility
@@ -27,6 +29,7 @@ export function createBoardUI(board) {
         } else {
            tile.setAttribute('cardId' , tileData.id); 
            tile.classList.add('normal');
+           tile.onclick = () => playerMoveOrActionModal(tileData.id);
            // Create and append the image element
            const img = document.createElement('img');
            img.src = `./assets/images/game-tiles/${tileData.slug}.jpeg`; // Adjust the extension if needed
