cf5ec57c975a50d6835c4e85176c5906cba41079
jasonsutter87 <jasonsutter87@gmail.com>
2024-09-11 23:37:37 -0700

    updating

--- client/static/src/js/game-machanics/cards.js
+++
@@ -1,8 +0,0 @@
-//Manages the game’s card deck, including drawing cards, shuffling, and handling special cards (e.g., treasure cards).
-import { shuffle, dividedShuffle } from "./shuffling.js";
-
-export let moveCardNewPile = (inbound, outbound) => {
-    if (outbound.length > 0) {
-      inbound.push(outbound.shift());
-    }
-};
