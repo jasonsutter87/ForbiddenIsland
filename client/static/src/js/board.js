d3edb391ebc8ee53734e14c94a0f36ca5b62f78d
jasonsutter87 <jasonsutter87@gmail.com>
2024-09-27 21:05:56 -0700

    testing

--- client/static/src/js/board.js
+++ client/static/src/js/board.js
@@ -33,6 +33,8 @@ let moveCardNewPile = (inbound, outbound) => {
 
 
 let floodSix = (room) => {
+  console.log(room)
+
     room.gameDetails.flood_deck.unused.forEach((val, ind) => {
         if(ind < 6) {
         let tile = selectObjectById(room.gameDetails.gameBoard, val.id)
